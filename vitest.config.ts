db8c9b2adbd946b324965516cbd06943cc3ef3f1
giito <giito0101@gmail.com>
2025-12-17 16:17:04 +0900

    feat(rtl-setup):rtlの起動確認

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    environment: "node", // まずは node でOK（React不要ならこれで一番ラク）
+    environment: "jsdom",
+    globals: true,
+    setupFiles: ["./vitest.setup.ts"],
   },
 });
